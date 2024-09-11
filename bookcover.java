95704ba3c0c5a1a16bc5a8e5498819f5ea38aea1
Sabk123 <770856@pdsb.net>
2024-09-11 17:02:15 -0400

    Update bookcover.java

--- bookcover.java
+++ bookcover.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("By Dr. Suess");
         System.out.println("One Fish\n" +
                 "<)))><\n" +
                 "Two Fish\n" +
