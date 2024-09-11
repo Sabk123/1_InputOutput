4511bed8ae3fafb66b8e66017d64db4bb9fa16d7
Sabk123 <770856@pdsb.net>
2024-09-11 17:03:22 -0400

    Create bunny.java

---
+++ bunny.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("A spring bunny:");
+        System.out.println("\\\\\n" +
+                "\\\\_\n" +
+                "( _\\\n" +
+                "/ \\__\n" +
+                "/ _/`\"`\n" +
+                "{\\ )_\n" +
+                "`\"\"\"`");
+    }
+}
